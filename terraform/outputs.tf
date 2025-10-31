3f55fc238fc47ccb32971cd4e4f7d0be2888c6c4
shayle664 <kuuh12@gmail.com>
2025-10-31 20:44:45 +0200

    commit

--- terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "public_ip"   { value = aws_instance.app.public_ip }
-output "app_url"     { value = "http://${aws_instance.app.public_ip}:${var.exposed_port}" }
-
--- terraform/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "aws_region" { type = string  default = "eu-central-1" }
-variable "subnet_id"  { type = string  description = "Existing subnet id (in a public subnet)" }
-variable "instance_type" { type = string default = "t3.micro" }
-variable "image" { type = string  description = "ghcr.io/shayle664/shay-final-project:latest" }
-variable "container_port" { type = number default = 5007 }
-variable "exposed_port"   { type = number default = 80 }
